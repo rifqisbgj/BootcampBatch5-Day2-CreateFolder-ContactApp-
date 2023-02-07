7348244b114af369ca3616e93d01ef4ab5ed1be1
Rifqi Subagja <rifqisubagjaa@gmail.com>
2023-02-07 12:04:31 +0700

    fix check file json

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ if (!fs.existsSync('./data')) {
     fs.mkdirSync('./data');
 }
 
-if (!fs.existsSync('./data/contacts.js')) {
+if (!fs.existsSync('./data/contacts.json')) {
     fs.writeFileSync('./data/contacts.json','[]');
 }
 
